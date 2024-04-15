5a034147155aae7620efd78ff4d4f2507bdb9395
Harshit-Aggarwal9 <harshit.aggarwal@scaler.com>
2024-04-15 23:24:04 +0530

    added the feature to complete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,6 @@
 class TodoItemsController < ApplicationController
     before_action :set_todo_list
-  
+    before_action :set_todo_item, except: [:create]
     def create
       @todo_item = @todo_list.todo_items.create(todo_item_params)
       redirect_to @todo_list
@@ -16,9 +16,10 @@ class TodoItemsController < ApplicationController
       redirect_to @todo_list
     end
   
-    # def show
-    #   @todo_item = TodoItem.find(params[:id])
-    # end
+    def complete
+        @todo_item.update_attribute(:completed_at, Time.now)
+        redirect_to @todo_list, notice: "Todo item complete"
+    end
   
     private
   
@@ -26,6 +27,10 @@ class TodoItemsController < ApplicationController
       @todo_list = TodoList.find(params[:todo_list_id])
     end
   
+    def set_todo_item
+        @todo_item = @todo_list.todo_items.find(params[:id])
+    end
+
     def todo_item_params
       params.require(:todo_item).permit(:content)
     end
